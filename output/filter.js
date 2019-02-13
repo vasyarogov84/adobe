ea56b728365106a0776b603e183ca289d239bfa1
vasyarogov84 <viktorgrom84@gmail.com>
2019-02-12 17:04:28 -0800

    initial commit

--- output/filter.js
+++ output/filter.js
@@ -9,15 +9,6 @@
     "index": 6
   },
   {
-    "_id": "wuj08238jdsnfsj23",
-    "email": "foo@bar.com",
-    "firstName": "Micah",
-    "lastName": "Valmer",
-    "address": "123 Street St",
-    "entryDate": "2014-05-07T17:33:20+00:00",
-    "index": 7
-  },
-  {
     "_id": "belr28238jdsnfsj23",
     "email": "mae@bar.com",
     "firstName": "Tallulah",
@@ -36,12 +27,21 @@
     "index": 9
   },
   {
-    "_id": "jkj238238jdsnfsj23",
-    "email": "coo@bar.com",
-    "firstName": "Ted",
-    "lastName": "Jones",
-    "address": "456 Neat St",
+    "_id": "qest38238jdsnfsj23",
+    "email": "foo@bar.com",
+    "firstName": "John",
+    "lastName": "Smith",
+    "address": "123 Street St",
     "entryDate": "2014-05-07T17:32:20+00:00",
-    "index": 3
+    "index": 5
+  },
+  {
+    "_id": "wabaj238238jdsnfsj23",
+    "email": "bog@bar.com",
+    "firstName": "Fran",
+    "lastName": "Jones",
+    "address": "8803 Dark St",
+    "entryDate": "2014-05-07T17:31:20+00:00",
+    "index": 2
   }
 ]
--- test.js
+++ test.js
@@ -3,28 +3,38 @@ let data = require("./data/leads.json");
 
 const test = data.leads.map((el, index) => ({ ...el, index }));
 
-let sortData = test
-  .sort((a, b) => Date.parse(b.entryDate) - Date.parse(a.entryDate));
-  
+let sortData = test.sort(
+  (a, b) =>
+    Date.parse(b.entryDate) - Date.parse(a.entryDate) && b.index - a.index
+);
+//console.log("SORT", sortData);
+
 function findIndex(value) {
-    let allIndexVal = sortData.filter(obj => obj.index).indexOf(value);
-    return allIndexVal;
+  let allIndexVal = sortData.map(obj => obj.index);
+  let i = allIndexVal.indexOf(value);
+  console.log(i);
+  return i;
 }
-  
 
 sortData.map(sd => {
-  test.map( d => {
+  test.map(d => {
     if (sd.email === d.email && sd.index !== d.index) {
-      const index = findIndex(d.index);  
+      const index = findIndex(d.index);
+      let logsData = `${d._id} was deleted for duplication policy \n`;
+      fs.appendFile("./logs/message.txt", logsData , function(err) {
+        if (err) throw err;
+        console.log("Saved!");
+      });
       sortData.splice(index, 1);
     } else if (sd._id === d._id && sd.index !== d.index) {
-        const index = findIndex(d.index);  
-        sortData.splice(index, 1);
+      const index = findIndex(d.index);
+      sortData.splice(index, 1);
     }
-    return d;
   });
-  return sd;
 });
 
-
-fs.writeFileSync("./output/filter.js", JSON.stringify(sortData, null, 2) , 'utf-8');
+fs.writeFileSync(
+  "./output/filter.js",
+  JSON.stringify(sortData, null, 2),
+  "utf-8"
+);
