bee3baed74ab50afe40fceab585f8450fcce8560
vasyarogov84 <viktorgrom84@gmail.com>
2019-02-13 15:04:51 -0800

    changed variables to proper names

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const fs = require("fs");
 let data = require("./data/leads.json");
 const loggerCreation = require("./helpers/loggerCreation");
 
-let test = data.leads.map((el, index) => ({ ...el, index }));
+fs.truncate("./logs/duplicate_users.txt", 0, () => {
+  console.log("duplicate_users.txt empty");
+});
+
+let dataWithIndex = data.leads.map((el, index) => ({ ...el, index }));
 
 let sortData = data.leads
   .map((el, index) => ({ ...el, index }))
   .sort((a, b) => {
-    return (
-      Date.parse(b.entryDate) === Date.parse(a.entryDate) ? 
-        b.index - a.index : Date.parse(b.entryDate) - Date.parse(a.entryDate)
-    );
+    return Date.parse(b.entryDate) === Date.parse(a.entryDate)
+      ? b.index - a.index
+      : Date.parse(b.entryDate) - Date.parse(a.entryDate);
   });
-  console.log(test);
+
 function findIndex(value) {
   let allIndexVal = sortData.map(obj => obj.index);
   let i = allIndexVal.indexOf(value);
@@ -20,29 +23,29 @@ function findIndex(value) {
 }
 
 sortData
-  .map(sortedData => {
-    test.map(indexedData => {
-      let { _id, email, index } = indexedData;
+  .map(user => {
+    dataWithIndex.map(indexed_user => {
+      let { _id, email, index } = indexed_user;
 
-      if (sortedData.email === email && sortedData.index !== index) {
+      if (user.email === email && user.index !== index) {
         const userIndexToDelete = findIndex(index);
         const reason = "EMAIL DUPLICATION";
-        const loggerData = loggerCreation(sortedData, indexedData, reason);
+        const loggerData = loggerCreation(user, indexed_user, reason);
         fs.appendFile("./logs/duplicate_users.txt", loggerData, err => {
           if (err) throw err;
         });
         sortData.splice(userIndexToDelete, 1);
-      } else if (sortedData._id === _id && sortedData.index !== index) {
+      } else if (user._id === _id && user.index !== index) {
         const userIndexToDelete = findIndex(index);
         const reason = "ID DUPLICATION";
-        const loggerData = loggerCreation(sortedData, indexedData, reason);
+        const loggerData = loggerCreation(user, indexed_user, reason);
         fs.appendFile("./logs/duplicate_users.txt", loggerData, err => {
           if (err) throw err;
         });
         sortData.splice(userIndexToDelete, 1);
       }
     });
-    return sortedData;
+    return user;
   })
   .map(valid_customer => {
     delete valid_customer.index;
